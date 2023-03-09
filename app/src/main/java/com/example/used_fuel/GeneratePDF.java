9513a82f485aac302361fc283426dfaf0a5e248c
Samir <Samilr.sg2@gmail.com>
2023-03-09 11:37:45 -0400

    Added: Data into a Tabla on PDF

--- app/src/main/java/com/example/used_fuel/GeneratePDF.java
+++ app/src/main/java/com/example/used_fuel/GeneratePDF.java
@@ -1,12 +1,27 @@
 package com.example.used_fuel;
 
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Typeface;
+import android.graphics.pdf.PdfDocument;
 import android.os.Environment;
+import android.text.TextPaint;
 import android.widget.Toast;
 
+import com.itextpdf.text.BaseColor;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.Element;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.FontFactory;
+import com.itextpdf.text.Image;
 import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
+
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.text.SimpleDateFormat;
@@ -43,20 +58,42 @@ public class GeneratePDF {
             this.pdfFileName = "CONSUMO_DE_COMBUSTIBLE_" + currrentDate;
             String path = Environment.getExternalStorageDirectory().getAbsolutePath() + "/Android/data/com.example.used_fuel/files/" + this.pdfFileName + ".pdf";
             PdfWriter.getInstance(document, new FileOutputStream(path));
+            PdfPTable table = new PdfPTable(2);
+           // Image image = Image.getInstance("ruta/de/tu/imagen.png");
+
             document.open();
 
-            // Agrega el título "REPORTE"
-            Paragraph title = new Paragraph("Reporte de Consumo " + currrentDate);
-            title.setAlignment(Element.ALIGN_CENTER);
-            document.add(title);
+            Paragraph title = new Paragraph("CONSUMO DE COMBUSTIBLE SG " + currrentDate);
+            Font boldFont = new Font(Font.FontFamily.TIMES_ROMAN, 18, Font.BOLD);
 
-            // Agrega los datos de las variables
-            document.add(new Paragraph("Fecha: " + currrentDate));
-            document.add(new Paragraph("CONSUMO KM/G: " +  _consumoKMG + " KM/G"));
-            document.add(new Paragraph("CONSUMO MI/G: " + _consumoMIG + " MI/G"));
-            document.add(new Paragraph("CONSUMO KM/L: " + _consumoKML + " KM/L"));
-            document.add(new Paragraph("COMBUSTIBLE USADO: " + _combustibleUsado + " GALONES"));
-            document.add(new Paragraph("DINERO USADO: RD$" + _dineroUsado + " DOP"));
+            title.setAlignment(Element.ALIGN_CENTER);
+            title.getFont().setSize(20f);
+            title.setFont(boldFont);
+
+            document.add(title);
+            document.add(new Paragraph(" "));
+
+            Font tableFont = FontFactory.getFont(FontFactory.TIMES_ROMAN, 14, Font.NORMAL, BaseColor.BLACK);
+
+            table.addCell("FECHA:");
+            table.addCell(currrentDate);
+
+            table.addCell("CONSUMO KM/G:");
+            table.addCell(_consumoKMG + " KM/G");
+
+            table.addCell("CONSUMO MI/G:");
+            table.addCell(_consumoMIG + " MI/G");
+
+            table.addCell("CONSUMO KM/L:");
+            table.addCell(_consumoKML + " KM/L");
+
+            table.addCell("COMBUSTIBLE USADO:");
+            table.addCell(_combustibleUsado + " GALONES");
+
+            table.addCell("DINERO USADO:");
+            table.addCell("RD$ " + _dineroUsado + " DOP");
+
+            document.add(table);
             document.close();
 
         }catch (SecurityException | FileNotFoundException e){
